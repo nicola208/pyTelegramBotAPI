3a61c941f4563e0790fa604f37c66ce850070772
nicola208 <77609781+nicola208@users.noreply.github.com>
2021-01-18 09:41:19 +0100

    Update echo_bot.py

--- examples/echo_bot.py
+++ examples/echo_bot.py
@@ -5,7 +5,7 @@
 
 import telebot
 
-API_TOKEN = '<api_token>'
+API_TOKEN = '1595206639:AAGDTFg_xRpHh6q9wjYV-JSESN9MZLyvuks'
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -14,15 +14,18 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+Ciao io sono il bot del gruppo"STW_Nico*_* e STW_Gionny.
+Sono stato appena creato per gestire il gruppo😅\
 """)
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
-
-
+    if.message.text=='Ciao':
+        bot.reply_to(message, Ciao sono un bot,come stai?')
+    elif message.text=='niente':
+        bot.reply_to(message, 'Bene mi fa piacere. Anche io ok!')
+    elise:
+        bot.reply_to(message, 'Sono stato appena creato e non capisco tutti i messaggi.Prova a scrivermi altro.')
 bot.polling()
